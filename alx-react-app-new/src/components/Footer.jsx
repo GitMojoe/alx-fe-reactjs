841dc7949cf83450cbf7826c91f13b9c935587c7
GitMojoe <greatestmojoe.com>
2025-11-09 18:07:48 +0000

    added new react app with inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer =()=>{
+    return(
+        <footer style={{backgroundColor:'navy'}}>
+            <p style={{color:'white'}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+const MainContent =()=>{
+    return(
+        <main style={{backgroundColor:'greenyellow'}}>
+            <p style={{color:'black', fontFamily:'sans-serif'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+export default MainContent;
