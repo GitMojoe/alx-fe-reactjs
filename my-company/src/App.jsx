62f833e274f80f9836cb4355074d9ea6a1208799
GitMojoe <greatestmojoe.com>
2025-11-09 22:52:15 +0000

    update

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
@@ -8,7 +8,14 @@ function App(){
 
     return(
       <>
-        
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Contact' element={<Contact />} />
+            <Route path='/About' element={<About />} />
+            <Route path='/Services' element={<Services />} />
+          </Routes>
+        </Router>
       </>
     )
 }
