539d5f859a235da0c79a5d6f778dca4d49776240
GitMojoe <greatestmojoe.com>
2025-11-16 22:44:13 +0000

    update

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,9 @@
+import {create} from "zustand"; 
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (recipeId) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== recipeId) })),
+  updateRecipe: (recipeId, updatedRecipe) => set(state => ({ recipes: state.recipes.map(recipe => recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe) }))
+}));
