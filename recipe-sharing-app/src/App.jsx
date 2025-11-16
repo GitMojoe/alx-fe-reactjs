bf94be7acf55ba2a822cdc92bcd8d8a276bdf32e
GitMojoe <greatestmojoe.com>
2025-11-16 18:50:28 +0000

    update

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import RecipeList from './Component/RecipeList';
+import AddRecipeListForm from './Component/AddRecipeForm';
+
+function App(){
+  return(
+    <>
+      <RecipeList />
+      <AddRecipeListForm />
+    </>
+  )
+}
+
+export default App;
