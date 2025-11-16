b641138b82d0a80d2ee6dc4a9ca87db1e29c9c62
GitMojoe <greatestmojoe.com>
2025-11-16 23:59:44 +0000

    update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,15 @@
- const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
- export default UserProfile;
+import React, { useContext } from 'react';
+import UserContext from './UserContext.js';
+
+const UserProfile = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
