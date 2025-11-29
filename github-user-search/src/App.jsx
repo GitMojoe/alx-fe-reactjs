71d00de4203965f0566a56fe7eb7384e347cd17f
GitMojoe <greatestmojoe.com>
2025-11-29 13:59:19 +0000

    update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,10 @@
 // src/App.jsx
 import Search from "./components/Search";
-import './output.css'
+
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
-        <h1 className="text-4xl font-bold text-blue-500">Tailwind is working!</h1>
       <Search />
     </div>
   );
